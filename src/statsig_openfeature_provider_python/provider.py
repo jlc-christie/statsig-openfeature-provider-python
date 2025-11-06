b4b3f4720aeee94d1f6401f7bd5165f5a6d38a71
james <jlc_christie@live.com>
2025-11-06 15:55:13 +0000

    add unimplemented provider

---
+++ src/statsig_openfeature_provider_python/provider.py
@@ -0,0 +1,50 @@
+import typing
+from typing import Sequence, Mapping
+
+from openfeature.evaluation_context import EvaluationContext
+from openfeature.flag_evaluation import FlagValueType, FlagResolutionDetails
+from openfeature.provider import AbstractProvider, Metadata
+
+
+class StatsigProvider(AbstractProvider):
+    def get_metadata(self) -> Metadata:
+        raise NotImplementedError
+
+    def resolve_boolean_details(
+        self, flag_key: str,
+        default_value: bool,
+        evaluation_context: typing.Optional[EvaluationContext] = None
+    ) -> FlagResolutionDetails[bool]:
+        raise NotImplementedError
+
+    def resolve_string_details(
+        self,
+        flag_key: str,
+        default_value: str,
+        evaluation_context: typing.Optional[EvaluationContext] = None
+    ) -> FlagResolutionDetails[str]:
+        raise NotImplementedError
+
+    def resolve_integer_details(
+        self,
+        flag_key: str,
+        default_value: int,
+        evaluation_context: typing.Optional[EvaluationContext] = None
+    ) -> FlagResolutionDetails[int]:
+        raise NotImplementedError
+
+    def resolve_float_details(
+        self,
+        flag_key: str,
+        default_value: float,
+        evaluation_context: typing.Optional[EvaluationContext] = None
+    ) -> FlagResolutionDetails[float]:
+        raise NotImplementedError
+
+    def resolve_object_details(
+        self,
+        flag_key: str,
+        default_value: typing.Union[Sequence[FlagValueType], Mapping[str, FlagValueType]],
+        evaluation_context: typing.Optional[EvaluationContext] = None
+    ) -> FlagResolutionDetails[typing.Union[Sequence[FlagValueType], Mapping[str, FlagValueType]]]:
+        raise NotImplementedError
